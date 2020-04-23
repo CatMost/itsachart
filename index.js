399202ce72a522b5c5d24d5257b92d690c273d4c
catherine most <catherinemost@gmail.com>
2020-04-22 23:00:03 -0700

    created css and js files

---
+++ index.js
@@ -0,0 +1,9 @@
+//this is a javascript file
+
+
+d3.select(h1).style('color','blue');
+.attr('id','heading')
+.text('Updated HELLO');
+
+d3.select('body').append('p')
+.text('this is a new paragraph')
